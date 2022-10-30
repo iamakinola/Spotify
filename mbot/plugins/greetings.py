2468ac262071e0da3755582f3202146788724150
olaniyi akinola <38849125+iamakinola@users.noreply.github.com>
2022-10-30 17:41:54 +0100

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -80,7 +80,7 @@ async def help(_,message):
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
 
-    await message.reply_text(f"Hello **{message.from_user.first_name}**, I'm **@syzipybot**.\nI'm Here to download your music.",
+    await message.reply_text(f"Hello **{message.from_user.first_name}**, I'm a Simple Music Downloader Bot. I Currently Support Download from Spotify, Youtube and Deezer.",
                         reply_markup=InlineKeyboardMarkup(button))
 
 @Mbot.on_callback_query(filters.regex(r"help_(.*?)"))
@@ -95,5 +95,5 @@ async def help_home(_,query):
     button = [
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
-    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@syzipybot**.\nI'm Here to download your music.",
+    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm a Simple Music Downloader Bot. I Currently Support Download from Spotify, Youtube and Deezer.",
                         reply_markup=InlineKeyboardMarkup(button))
